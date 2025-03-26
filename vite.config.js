c029d0c615f3609dc8fd7f6620bc555af1334c5b
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-26 22:57:39 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import process from "process";
+console.log(process.env.npm_package_name);
+// const appName = process.env.pwd.split("/").pop();
+export default defineConfig({
+  // Vite'nin proje kök dizinini belirtir ve GitHub Pages'te yayınlamak için gerekli olan alt dizini belirtir
+  base:
+    process.env.NODE_ENV === "development"
+      ? "./"
+      : `/${process.env.npm_package_name}/`,
+  // Vite'nin proje kök dizini olarak 'src' klasörünü kullanmasını sağlar
+  root: "./src/",
+  // Sunucu Ayarları
+  server: {
+    // Tarayıcı Başlat
+    open: true,
+    // Port Ayarla (Varsayılan: 5173)
+    port: 9000,
+    // Dış Bağlantıları Kabul Et
+    cors: false,
+  },
+  // Çıkış Ayarları
+  build: {
+    // Çıktıyı proje kökünde 'dist' klasörüne alır
+    outDir: "../dist",
+    // Çıktı klasörünü temizler
+    emptyOutDir: true,
+  },
+});
